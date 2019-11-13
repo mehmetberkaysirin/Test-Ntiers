c3f624718ba76cf551af223b384c7b8c338738fe
mehmetberkaysirin <mehmetberkaysirin@gmail.com>
2019-11-13 11:00:28 +0300

    Add project files.

---
+++ Test.DataAccess/Migrations/20191112140229_fsf.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test.DataAccess.Migrations
+{
+    public partial class fsf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emlak",
+                columns: table => new
+                {
+                    EmlakId_PK = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmlakTur = table.Column<string>(nullable: true),
+                    Metrekare = table.Column<int>(nullable: false),
+                    OdaSayisi = table.Column<int>(nullable: false),
+                    Kat = table.Column<int>(nullable: false),
+                    BinaKati = table.Column<int>(nullable: false),
+                    IsinmaTur = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emlak", x => x.EmlakId_PK);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Isyeri",
+                columns: table => new
+                {
+                    IsYeriId_PK = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    isletmeAdi = table.Column<string>(nullable: true),
+                    Yetkili = table.Column<string>(nullable: true),
+                    Adres = table.Column<string>(nullable: true),
+                    Telefon = table.Column<string>(nullable: true),
+                    Fax = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Isyeri", x => x.IsYeriId_PK);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musteri",
+                columns: table => new
+                {
+                    MusteriId_PK = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Adi = table.Column<string>(nullable: true),
+                    Soyadi = table.Column<string>(nullable: true),
+                    EvTel = table.Column<string>(nullable: true),
+                    CepTel = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Adres = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musteri", x => x.MusteriId_PK);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    EMail = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emlak");
+
+            migrationBuilder.DropTable(
+                name: "Isyeri");
+
+            migrationBuilder.DropTable(
+                name: "Musteri");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
